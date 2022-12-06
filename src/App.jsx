e3e94e7ea3acc2d7aeeabfa11b30768421f22cba
Jose Daniel <jossolpue@alum.us.es>
2022-12-06 13:37:59 +0100

    Test4

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Navbar, Container, Nav } from "solid-bootstrap";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Navbar bg="light" variant="light" fixed="top">
+      <Container>
+        <Navbar.Brand href="#">
+          <img alt="" src={"logo.svg"} width="160" height="90" />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse class="justify-content-end">
+            <Nav.Link href="#" class="rightBorderGray">Home</Nav.Link>
+            <Nav.Link href="#" class="rightBorderGray">Fromación Académica</Nav.Link>
+            <Nav.Link href="#" class="rightBorderGray">Exp. Profesional</Nav.Link>
+            <Nav.Link href="#" class="rightBorderGray">¿Por qué yo?</Nav.Link>
+            <Nav.Link href="#" class="rightBorderGray">Contáctame</Nav.Link>
+            <div id="google_translate_element"></div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
